e2c906093ef2b0495c816d9f710c020e53087f67
Evrevolt <gennadi_wow@mail.ru>
2022-05-29 17:06:06 +0300

    Initial Commit

---
+++ TConverter/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Геннадий Ведерников on 17.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var celsiesLabel: UILabel!
+    @IBOutlet weak var faringatesLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        
+        let temperatureCelsion = Int(round(sender.value))
+        celsiesLabel.text = "\(temperatureCelsion)ºC"
+        
+        faringatesLabel.text = "\(temperatureCelsion * 9 / 5 + 32)ºF"
+        
+//        if temperatureCelsion < -6 {
+//            sender.thumbTintColor = .blue
+//        } else if temperatureCelsion > 6 {
+//            sender.thumbTintColor = .red
+//        } else {
+//            sender.thumbTintColor = .white
+//        }
+        
+    }
+    
+}
+
